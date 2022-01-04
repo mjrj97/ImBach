e17406202a351974b75d42b43d88c8729de55621
mjrj97 <martin.jensen.1997@hotmail.com>
2022-01-04 12:44:01 +0100

    Added sounds and buttons in canvas.

--- code.js
+++ code.js
@@ -1,16 +1,35 @@
+// Interfaces
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
-ctx.font = '36px serif';
+var player = new Audio();
 
-distance = 10;
-correct = 0;
-total = 0;
+// Classes
+class Button {
+    constructor(rect, text) {
+        this.rect = rect;
+        this.text = text;
+    }
+}
 
-document.addEventListener("keyup", keyUpHandler, false);
+// Constants
+const notes = ["a3", "b3", "c4", "d4", "e4", "f4", "g4", "a4", "b4", "c5", "d5", "e5", "f5", "g5", "a5", "b5", "c6"];
+const buttons = [
+    new Button(rect = {x: canvas.width / 2 - 60 * 3 - 25, y:canvas.height - 60, width:50, height:50},"C"),
+    new Button(rect = {x: canvas.width / 2 - 60 * 2 - 25, y:canvas.height - 60, width:50, height:50},"D"),
+    new Button(rect = {x: canvas.width / 2 - 60 * 1 - 25, y:canvas.height - 60, width:50, height:50},"E"),
+    new Button(rect = {x: canvas.width / 2 - 25, y:canvas.height - 60, width:50, height:50},"F"),
+    new Button(rect = {x: canvas.width / 2 + 60 * 1 - 25, y:canvas.height - 60, width:50, height:50},"G"),
+    new Button(rect = {x: canvas.width / 2 + 60 * 2 - 25, y:canvas.height - 60, width:50, height:50},"A"),
+    new Button(rect = {x: canvas.width / 2 + 60 * 3 - 25, y:canvas.height - 60, width:50, height:50},"B")
+];
 
-const notes = ["a", "b", "c", "d", "e", "f", "g", "a", "b", "c", "d", "e", "f", "g", "a", "b", "c"];
-currentNote = random(0,notes.length-1);
+// Values
+let distance = 15;
+let correct = localStorage.getItem("correct") == null ? 0 : localStorage.getItem("correct");
+let total = localStorage.getItem("total") == null ? 0 : localStorage.getItem("total");
+let currentNote = random(0,notes.length-1);
 
+// Images
 var gclef = new Image();
 gclef.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/GClef.svg/1200px-GClef.svg.png";
 gclef.onload = function() {  
@@ -20,6 +39,18 @@ gclef.onload = function() {
 
 var note = new Image();
 note.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/WholeNote.svg/2560px-WholeNote.svg.png";
+
+// Input
+document.addEventListener("keyup", keyUpHandler, false);
+canvas.addEventListener('click', function(evt) {
+    var mousePos = getMousePos(canvas, evt);
+
+    for (let i = 0; i < buttons.length; i++) {
+        if (isInside(mousePos, buttons[i].rect)) {
+            buttonHandler(buttons[i].text.toLowerCase());
+        }
+    }
+}, false);
 
 function keyUpHandler(e) {
     handleInput(e.key);
@@ -32,44 +63,70 @@ function buttonHandler(e) {
 function handleInput(e) {
     wasGuess = false;
     for (let i = 0; i < notes.length; i++) {
-        if (e == notes[i])
+        if (e == notes[i][0])
             wasGuess = true;
     }
 
-    if(e == notes[currentNote]) {
+    if(e == notes[currentNote][0]) {
+        player.src = "audio/" + notes[currentNote] + ".mp3";
+        player.play();
         currentNote = random(0,notes.length-1);
         correct++;
+      	localStorage.setItem("correct", correct);
     }
 
     if (wasGuess)
+    {
         total++;
+        localStorage.setItem("total", total);
+    }
 }
 
+function getMousePos(canvas, event) {
+    var rect = canvas.getBoundingClientRect();
+    return {
+        x: event.clientX - rect.left,
+        y: event.clientY - rect.top
+    };
+}
+
+// Utility
 function random(min, max) { 
     return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
+function isInside(pos, rect){
+    return pos.x > rect.x && pos.x < rect.x+rect.width && pos.y < rect.y+rect.height && pos.y > rect.y
+}
+
+// UI
 function drawSheet() {
     ctx.drawImage(gclef, 0, canvas.height/2 - (gclef.height/2 * distance/10), gclef.width * distance/10, gclef.height * distance/10);
-    ctx.drawImage(note, 100, canvas.height/2+distance*3.5-currentNote*(distance/2),17 * distance/10,10 * distance/10);
+    ctx.drawImage(note, 100 * distance/10, canvas.height/2+distance*3.5-currentNote*(distance/2),17 * distance/10,10 * distance/10);
+
+    ctx.textAlign = "start";
+    ctx.fillStyle = "black";
+    ctx.font = 'bold ' + 30*distance/10 + 'px Allegretto';
+    ctx.fillText(4, (gclef.width + 10) * distance/10, canvas.height/2);
+    ctx.fillText(4, (gclef.width + 10) * distance/10, canvas.height/2 + distance * distance/10 + distance/2);
 
     ctx.beginPath();
 
     if (currentNote > 13) {
-        ctx.moveTo(90, canvas.height/2 - distance * 3);
-        ctx.lineTo(127, canvas.height/2 - distance * 3);
+        ctx.moveTo(90 * distance/10, canvas.height/2 - distance * 3);
+        ctx.lineTo(127 * distance/10, canvas.height/2 - distance * 3);
     }
     if (currentNote > 15) {
-        ctx.moveTo(90, canvas.height/2 - distance * 4);
-        ctx.lineTo(127, canvas.height/2 - distance * 4);
+        ctx.moveTo(90 * distance/10, canvas.height/2 - distance * 4);
+        ctx.lineTo(127 * distance/10, canvas.height/2 - distance * 4);
     }
     if (currentNote < 3) {
-        ctx.moveTo(90, canvas.height/2 + distance * 3);
-        ctx.lineTo(127, canvas.height/2 + distance * 3);
+        ctx.moveTo(90 * distance/10, canvas.height/2 + distance * 3);
+        ctx.lineTo(127 * distance/10, canvas.height/2 + distance * 3);
     }
     if (currentNote < 1) {
-        ctx.moveTo(90, canvas.height/2 + distance * 4);
-        ctx.lineTo(127, canvas.height/2 + distance * 4);
+        ctx.moveTo(90 * distance/10, canvas.height/2 + distance * 4);
+        ctx.lineTo(127 * distance/10, canvas.height/2 + distance * 4);
     }
 
     for (let i = 0; i < 5; i++) {
@@ -80,13 +137,32 @@ function drawSheet() {
 }
 
 function drawPoints() {
-    ctx.fillText(correct + "/" + total, 10, 50);
+    ctx.textAlign = "start";
+    ctx.fillStyle = "black";
+    ctx.font = 'bold 36px Allegretto';
+    ctx.fillText(correct + "/" + total, 20, 50);
+}
+
+function drawButtons() {
+    ctx.beginPath();
+    for (let i = 0; i < buttons.length; i++) {
+        ctx.fillStyle = "gray";
+        ctx.fillRect(buttons[i].rect.x, buttons[i].rect.y, buttons[i].rect.width, buttons[i].rect.height);
+        ctx.strokeStyle = "black";
+        ctx.rect(buttons[i].rect.x, buttons[i].rect.y, buttons[i].rect.width, buttons[i].rect.height);
+        ctx.textAlign = "center";
+        ctx.fillStyle = "black";
+        ctx.font = 'bold 36px Allegretto';
+        ctx.fillText(buttons[i].text, buttons[i].rect.x + buttons[i].rect.width/2, buttons[i].rect.y+buttons[i].rect.height - 12);
+    }
+    ctx.stroke();
 }
 
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawSheet();
     drawPoints();
+    drawButtons();
 }
 
 setInterval(draw, 10);
